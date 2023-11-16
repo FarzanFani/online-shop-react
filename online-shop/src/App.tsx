62f6d945b6c84dc5668b4d3096c92ead7e61be9a
Farzan Fani <mrm776384@gmail.com>
2023-11-16 04:55:09 +0330

    *.tsx

--- online-shop/src/App.tsx
+++ online-shop/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import SearchItems from "./Components/ProductsPage/SearchItems";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchItems />
     </div>
   );
 }
