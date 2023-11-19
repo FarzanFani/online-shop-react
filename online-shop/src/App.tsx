67365b863c22902ec156e6116c5b52f15aa5dd25
Farzan Fani <mrm776384@gmail.com>
2023-11-19 12:48:18 +0330

    fixing spacing problem

--- online-shop/src/App.tsx
+++ online-shop/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import ProductCardsContainer from "./Components/ProductsPage/ProductCardsContainer";
+import SearchItems from "./Components/ProductsPage/SearchItems";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchItems />
+      <ProductCardsContainer />
     </div>
   );
 }
